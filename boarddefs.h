2ad57ed5e6e396659962acca85fac72c5ae58aa9
Grrarth <45907442+Grrarth@users.noreply.github.com>
2018-12-16 13:06:17 +1000

    Update boarddefs.h

--- boarddefs.h
+++ boarddefs.h
@@ -84,13 +84,13 @@
         // avr/interrupt.h is not present
 #       undef HAS_AVR_INTERRUPT_H
 
-	// BEGIN - GarthClarkson - 20181213
+	// BEGIN - Grrarth@GitHub - 20181213
 	// Arduino 1.8.5 (ESP32 Selected) will not compile because it needs 
 	// a send pin even though not used due to no support
 	const int SEND_PIN = 0;
-	// END - GarhClarkson - 20181213
+	// END - Grrarth@GitHub - 20181213
 
-        // Sending not implemented
+// Sending not implemented
 #       undef SENDING_SUPPORTED#
 
         // Supply own enbleIRIn
